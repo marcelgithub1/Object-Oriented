99702d865bfe8af9b9d3645595c13be04a73a597
Marcelo Augusto <76758205+marcelgithub1@users.noreply.github.com>
2021-01-31 02:51:12 -0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -9,6 +9,9 @@ const countMarcelo = new CheckingCount(client1, 302);
 
 const countManu = new CheckingCount(client2, 2410);
 
+//Visualizando no console a conta Marcelo
 console.log(countMarcelo);
+//Visualizando no console a conta Manuella
 console.log(countManu);
+//Visualizando no console o contador
 console.log(CheckingCount.countAcc);
